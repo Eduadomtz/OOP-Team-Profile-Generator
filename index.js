cf024b1e1a2472b365e465775af24d07738ba8f2
Eduardo Martinez <eduardo31794@gmail.com>
2021-07-14 10:33:53 +0200

    Updating inquirer in index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const generateMarkdown = require('./dist/generateMarkdown');
+//const generateHTML = require('./dist/index');
 
 const Employee = require("./lib/Employee.js");
 const Engineer = require("./lib/Engineer.js");
@@ -29,62 +30,63 @@ const managerQuestions = [
         message: `What is the team manager's office number?`,
     },
 ];
+const employeeQuestions =
+    [
+        {
+            type: 'list',
+            name: 'memberType',
+            message: 'Which type of team member would you like to add?',
+            choices: ["Engineer", "Intern", "I don't want to add any more team members"],
+        },
+    ];
 
-const employeeQuestions = [
-    {
-        type: 'checkbox',
-        name: 'membertype',
-        message: 'Which type of team member would you like to add?',
-        choices: [`Engineer`, `Intern`, `I don't want to add any more team members`],
-    },
-    {
-        type: 'input',
-        name: 'XXXXXname',
-        message: `What is your XXXXXX name?`,
-    },
-    {
-        type: 'input',
-        name: 'XXXXXid',
-        message: `What is you XXXXXX id? `,
-    },
-    {
-        type: 'input',
-        name: 'XXXXXemail',
-        message: `What is you XXXXX email? `,
-    },
-    {
-        type: 'input',
-        name: 'XXXXgithub',
-        message: `What is you XXXXXX Github userman? `,
-    },
-    {
-        type: 'checkbox',
-        name: 'membertype',
-        message: 'Which type of team member would you like to add?',
-        choices: [`Engineer`, `Intern`, `I don't want to add any more team members`],
-    },
-    {
-        type: 'input',
-        name: 'XXXXXname',
-        message: `What is your XXXXXX name?`,
-    },
-    {
-        type: 'input',
-        name: 'XXXXXid',
-        message: `What is you XXXXXX id? `,
-    },
-    {
-        type: 'input',
-        name: 'XXXXXemail',
-        message: `What is you XXXXX email? `,
-    },
-    {
-        type: 'input',
-        name: 'XXXXgithub',
-        message: `What is you XXXXXX Github userman? `,
-    },
-];
+const engineerQuestions =
+    [
+        {
+            type: 'input',
+            name: 'EngineerName',
+            message: `What is your Engineer name?`,
+        },
+        {
+            type: 'input',
+            name: 'EngineerId',
+            message: `What is you Engineer id? `,
+        },
+        {
+            type: 'input',
+            name: 'EngineerEmail',
+            message: `What is you Engineer email? `,
+        },
+        {
+            type: 'input',
+            name: 'EmployeesGithub',
+            message: `What is you Engineer Github userman? `,
+        },
+    ];
 
+const internQuestions =
+    [
+        {
+            type: 'input',
+            name: 'InternName',
+            message: `What is your Intern name?`,
+        },
+        {
+            type: 'input',
+            name: 'InternId',
+            message: `What is you Intern id? `,
+        },
+        {
+            type: 'input',
+            name: 'InternEmail',
+            message: `What is you Intern email? `,
+        },
+        {
+            type: 'input',
+            name: 'InternGithub',
+            message: `What is you Intern Github userman? `,
+        }
+    ];
 
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, data, err => {
@@ -95,12 +97,35 @@ function writeToFile(fileName, data) {
     });
 };
 
-function init() { 
-    inquirer.prompt(questions)
-    .then(function (userAnswer) {
-        console.log(userAnswer)
-        writeToFile("index.html", generateMarkdown(userAnswer));
-    });
+function init() {
+    inquirer.prompt(managerQuestions)
+        .then(function (userAnswer) {
+            console.log(userAnswer)
+            //writeToFile("index.html", generateMarkdown(userAnswer));
+            newMember();
+        });
+}
+
+function newMember() {
+    inquirer.prompt(employeeQuestions)
+        .then((userAnswer) => {
+
+            switch (userAnswer.memberType) {
+                case "Engineer":
+                    inquirer.prompt(engineerQuestions).then(userAnswer);
+                    break;
+                    console.log(userAnswer)
+                case "Intern":
+                    inquirer.prompt(internQuestions).then(userAnswer);
+                    console.log(userAnswer)
+                default:
+                    break;
+
+            }
+
+            // writeToFile("index.html", generateMarkdown(userAnswer));
+        });
+
 }
 
 
